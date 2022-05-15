c16671415eb8b205a519edb6d30423f09e372400
AivazNurgaliev <aivaznurgaliev@gmail.com>
2022-05-15 09:23:22 +0300

    Test commit

--- project/src/main/java/com/ourproject/portfoliotracker/PortfoliotrackerApplication.java
+++ project/src/main/java/com/ourproject/portfoliotracker/PortfoliotrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfoliotrackerApplication {
-
+	//для проверки коммита и пуша моей ветки, удалим потом
 	public static void main(String[] args) {
 		SpringApplication.run(PortfoliotrackerApplication.class, args);
 	}
