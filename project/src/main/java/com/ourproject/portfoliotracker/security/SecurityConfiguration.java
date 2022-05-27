eb898285990a780045f388e4770f75070a769735
vaempunk <vmkaaidiyatov@gmail.com>
2022-05-27 21:02:16 +0300

    fix: ant matcher to mvc

--- project/src/main/java/com/ourproject/portfoliotracker/security/SecurityConfiguration.java
+++ project/src/main/java/com/ourproject/portfoliotracker/security/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers(
+                .mvcMatchers(
                         "/", "/overview", "/register", "/restore")
                 .permitAll()
                 .anyRequest().authenticated()
